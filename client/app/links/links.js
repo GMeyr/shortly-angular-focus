907165d78e713d3c0d2dc337946a8313acc64ab1
GMeyr <gabrielmeyr@gmail.com>
2015-12-30 22:28:37 -0500

    made links a property of $scope

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,13 +1,11 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
-  // gabe: in here we use the methods defined on our Links object
-  //       to handle our link logic and rendering
+  $scope.data = {};
   console.log('Link Controller Loaded');
   $scope.getLinks = function() {
-    Links.getLinks().then(function(data) {
-      $scope.data = data;
+    Links.getLinks().then(function(links) {
+      $scope.data.links = links;
     });
   }
   $scope.getLinks();
